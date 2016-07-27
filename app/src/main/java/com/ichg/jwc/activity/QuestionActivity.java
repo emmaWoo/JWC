eb61ebe26c6182b31961b091c1575c44c8665ea9
jasonho <jasonho@kkbox.com>
2016-07-27 23:07:24 +0800

    Modify question UI and fix some issue

--- app/src/main/java/com/ichg/jwc/activity/QuestionActivity.java
+++ app/src/main/java/com/ichg/jwc/activity/QuestionActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.ichg.jwc.R;
 import com.ichg.jwc.manager.ToolbarManager;
@@ -29,6 +30,25 @@ public class QuestionActivity extends ActivityBase {
 
     @OnClick(R.id.label_phone)
     public void onClick() {
-        startActivity(new Intent("android.intent.action.DIAL", Uri.parse("tel:xxxxxxxxxx")));
+        startActivity(new Intent("android.intent.action.DIAL", Uri.parse("tel:02-2225-4066")));
+    }
+
+    @OnClick(R.id.label_email)
+    public void onClickEmail() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"jw.service@joinworker.com.tw"});
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick(R.id.label_question)
+    public void onClickQuestion() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.ichg.com.tw/ezcatfiles/EC-SBU008/img/img/1335/joinwork_qa.html"));
+        startActivity(browserIntent);
     }
 }
